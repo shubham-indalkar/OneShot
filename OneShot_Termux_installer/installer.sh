c9ecabee6a83ca57e9305543e2a6dd2f1b85e934
Shubham Indalkar <djsi55555@gmail.com>
2025-08-24 20:06:28 +0530

    Update installer.sh
    Create a OneShot directory first and then download into it

--- OneShot_Termux_installer/installer.sh
+++ OneShot_Termux_installer/installer.sh
@@ -2,20 +2,25 @@
 architecture=""
 case $(uname -m) in
     armv7*)   architecture="arm" ;;
-    aarch64)   architecture="aarch64" ;;
-    *)   printf "The architecture of your CPU is not supported. Please build required binaries from the sources: https://github.com/shubham-indalkar/OneShot#termux"; exit
+    aarch64)  architecture="aarch64" ;;
+    *)   printf "The architecture of your CPU is not supported. Please build required binaries from the sources: https://github.com/drygdryg/OneShot#termux"; exit
 esac
+
 pkg install -y root-repo 
 pkg install -y wget tsu python wpa-supplicant iw
 
+# Create OneShot folder and move into it
+mkdir -p OneShot
+cd OneShot
+
 # keep upstream
-wget https://raw.githubusercontent.com/shubham-indalkar/Oneshot/master/OneShot_Termux_installer/binaries/$architecture/pixiewps -O $PREFIX/bin/pixiewps
+wget https://raw.githubusercontent.com/drygdryg/OneShot_Termux_installer/master/binaries/$architecture/pixiewps -O $PREFIX/bin/pixiewps
 chmod +x $PREFIX/bin/pixiewps
 
-wget https://raw.githubusercontent.com/shubham-indalkar/OneShot/master/oneshot.py
+wget https://raw.githubusercontent.com/eda-abec/OneShot-Termux/termux/oneshot.py
 # keep upstream
-wget https://raw.githubusercontent.com/shubham-indalkar/OneShot/master/vulnwsc.txt
+wget https://raw.githubusercontent.com/drygdryg/OneShot/master/vulnwsc.txt
 
 chmod +x ./oneshot.py
 
-printf "########################################\n# All done! Now you can run OneShot with\n#        sudo python oneshot.py\n########################################\n"
+printf "########################################\n# All done! Now you can run OneShot with\n#        sudo python OneShot/oneshot.py\n########################################\n"
